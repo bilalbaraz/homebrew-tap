9337c7c38e25e7c461b7f449d400156767fd05ec
Bilal Baraz <bilalbaraz@windowslive.com>
2026-01-31 01:25:10 +0300

    Update sha256 and remove renaming

--- Formula/passgen.rb
+++ Formula/passgen.rb
@@ -2,11 +2,11 @@ class Passgen < Formula
   desc "Secure password generator written in Go"
   homepage "https://github.com/bilalbaraz/passgen"
   url "https://github.com/bilalbaraz/passgen/releases/download/v0.1.0/passgen_darwin_amd64.zip"
-  sha256 "09f735757c1fd86c16048191352136c1802df7b8e1508bcd976de2c3c7b68995"
+  sha256 "10f72eccfc7a78feafc3cd9ad4a8354107cfd9590e3411ae0289e29cf137a7d9"
   version "0.1.0"
 
   def install
-    bin.install "passgen_darwin_amd64" => "passgen"
+    bin.install "passgen"
   end
 
   test do
