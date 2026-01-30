c9766bf1f9459167d284de40dd6802de36f06df9
Bilal Baraz <bilalbaraz@windowslive.com>
2026-01-31 01:03:37 +0300

    First commit, Add passgen formula

---
+++ Formula/passgen.rb
@@ -0,0 +1,14 @@
+  desc "Secure password generator written in Go"
+  homepage "https://github.com/bilalbaraz/passgen"
+  url "https://github.com/bilalbaraz/passgen/releases/download/v0.1.0/passgen_darwin_amd64.zip"
+  sha256 "09f735757c1fd86c16048191352136c1802df7b8e1508bcd976de2c3c7b68995"
+  version "0.1.0"
+
+  def install
+    bin.install "passgen"
+  end
+
+  test do
+    system "#{bin}/passgen", "-len", "8"
+  end
+end
---
+++ README.md
@@ -0,0 +1,31 @@
+# Bilal’s Homebrew Tap
+
+Homebrew tap for shipping my CLI tools and apps.
+
+## Install
+
+```bash
+brew tap bilalbaraz/tap
+```
+
+## Install Packages
+
+```bash
+# formula
+brew install bilalbaraz/tap/<name>
+```
+
+## Packages
+
+### Formulae
+
+- `passgen` — Fast CLI for generating secure, memorable passwords
+
+## Update / Uninstall
+
+```bash
+brew update
+brew upgrade
+
+brew uninstall <formula>
+```
