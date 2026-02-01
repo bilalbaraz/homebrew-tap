5fc49002289046bb321f38f575b67c3e579fb09d
Bilal Baraz <bilalbaraz@windowslive.com>
2026-02-01 18:24:29 +0300

    Add v0.2.0 release for passgen

--- Formula/passgen.rb
+++ Formula/passgen.rb
@@ -1,9 +1,9 @@
 class Passgen < Formula
   desc "Secure password generator written in Go"
   homepage "https://github.com/bilalbaraz/passgen"
-  url "https://github.com/bilalbaraz/passgen/releases/download/v0.1.0/passgen_darwin_amd64.zip"
-  sha256 "10f72eccfc7a78feafc3cd9ad4a8354107cfd9590e3411ae0289e29cf137a7d9"
-  version "0.1.0"
+  url "https://github.com/bilalbaraz/passgen/releases/download/v0.2.0/passgen_darwin_amd64.zip"
+  sha256 "60cbdab8bf86d6c7096fae2be3daf4a9b847d1c7d1960c772d6597f5c0d6d4a1"
+  version "0.2.0"
 
   def install
     bin.install "passgen"
