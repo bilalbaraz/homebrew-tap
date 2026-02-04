a0a5cd117e0a6e9af764d82e4efcf1574f503d69
Bilal Baraz <bilalbaraz@windowslive.com>
2026-02-05 00:50:32 +0300

    Update sha256

--- Formula/passgen.rb
+++ Formula/passgen.rb
@@ -2,7 +2,7 @@ class Passgen < Formula
   desc "Secure password generator written in Go"
   homepage "https://github.com/bilalbaraz/passgen"
   url "https://github.com/bilalbaraz/passgen/releases/download/v0.4.0/passgen_darwin_amd64.zip"
-  sha256 "1aaf64286c047ae2f950864244c1594507ef067ab1a2ed4f3b3ea013a077789c"
+  sha256 "5a0a05d84d3e7b334468d0dd2de4feb728274b49d7cd0e889c5e2d0ec26985b3"
   version "0.4.0"
 
   def install
